700e59382c55baf2f927dd621e1e0717f43bf8eb
murrehman <muteeurrehmanbajwa@gmail.com>
2022-04-07 02:07:27 +0200

    project Init

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { SingleEmployeeComponent } from './single-employee/single-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EmployeeComponent },
+  { path: 'singleEmployee/:id', component: SingleEmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { AuthComponent } from './auth/auth.component';
+import { SingleEmployeeComponent } from './single-employee/single-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    AuthComponent,
+    SingleEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [FormBuilder,
+    HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
